638630d4a8eec42a2b9d8ecab4a24a10d79361cf
lichsam <lichsam@hotmail.com>
2018-05-28 20:51:03 +0900

    Project2 Start
    add javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function view_project(project_name){
+	var window = document.getElementById('project_window');
+	var iframe = document.getElementById('project_iframe');
+	window.style.display = 'block';
+	iframe.src = "projects/"+project_name;
+}
+function exit_project(){
+	var window = document.getElementById('project_window');
+	window.style.display = 'none';
+}
