c0f8d8e9233045dbfedca2afd6297881e8c76eab
lichsam <lichsam@hotmail.com>
2018-06-18 16:21:36 +0900

    project3
    jQuery적용 및
    스크롤, 툴팁 등 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 function view_project(project_name){
-	var window = document.getElementById('project_window');
-	var iframe = document.getElementById('project_iframe');
-	window.style.display = 'block';
-	iframe.src = "https://lichsam.github.io/projects/"+project_name;
+	$('#project_window').css("display","block");
+	$('#project_iframe').attr('src', "https://lichsam.github.io/projects/"+project_name);
 }
 function exit_project(){
-	var window = document.getElementById('project_window');
-	var iframe = document.getElementById('project_iframe');
-	iframe.src = "";
-	window.style.display = 'none';
+	$('#project_window').css("display","none");
+	$('#project_iframe').attr('src','');
 }
+
 function copyMail(){
 	document.querySelector('#email').select();
 	document.execCommand('copy');
-	document.getElementById('copytext').innerHTML = "Copied";
+	$('#copytext').fadeToggle(100,function(){
+		$('#copytext').text('Copied');
+		$('#copybtn').css('border', '3px solid #FF8224');
+		$('#copybtn').css('color', '#FF8224');
+	});
+	$('#copytext').fadeToggle(200);
+}
+
+function scroll(menuid){
+	var offset = $("#" + menuid).offset();
+	$('html, body').animate({scrollTop : offset.top}, 700);
+}
+
+function mini_view(project_name){
+	$('.mini_view').css("display","block");
+	$('.mini_view img').attr("src","../images/mini_" + project_name + ".png");
+}
+function mini_exit(){
+	$('.mini_view').css("display","none");
 }
